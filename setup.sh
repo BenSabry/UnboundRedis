f1c57cea18c473241765128cad50aa857349a921
Ahmed Sabry <40901075+BenSabry@users.noreply.github.com>
2024-07-29 15:23:09 +0300

    Add config-reset

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ mkdir ~/scripts
 
 /bin/cp -f ./scripts/status.sh ~/scripts/status.sh
 /bin/cp -f ./scripts/monitor.sh ~/scripts/monitor.sh
-
 /bin/cp -f ./scripts/system-upgrade.sh ~/scripts/system-upgrade.sh
 /bin/cp -f ./scripts/unbound-update.sh ~/scripts/unbound-update.sh
+/bin/cp -f ./scripts/config-reset.sh ~/scripts/config-reset.sh
 /bin/cp -f ./scripts/unbound-update-roothints.sh ~/scripts/unbound-update-roothints.sh
 
 # make scripts executable
