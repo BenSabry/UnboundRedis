74617801aafa3086d11edd0b589faf454360bb7f
Ahmed Sabry <40901075+BenSabry@users.noreply.github.com>
2024-07-30 14:14:43 +0300

    Redis config update

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,14 +1,21 @@
-echo "https://github.com/BenSabry/UnboundRedis";
-echo "";
+# variables
+unboundStates="$(unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num)"
+unboundCachePercent="$(echo "$(unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num.cachehits= | cut -c 21-)/$(unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num.queries= | cut -c 19-)" | bc -l | cut -c 2-3;)"
 
-echo -n "unbound"; service unbound status;
-echo "-------------------------";
+redisStates="$(redis-cli info memory | grep 'used.*human')";
+redisDbSize="$(redis-cli dbsize)"
 
-echo -n "total.percent.cachehits="; echo -n "$(echo "$(unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num.cachehits= | cut -c 21-)/$(unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num.queries= | cut -c 19-)" | bc -l | cut -c 2-3;)"; echo "%"; 
+# messages
+echo "https://github.com/BenSabry/UnboundRedis"
+echo ""
 
-unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num; echo ""; 
+echo -n "unbound"; service unbound status
+echo "-------------------------"
+echo "total.percent.cachehits=$unboundCachePercent%"
+echo "$unboundStates"
+echo ""
 
-echo -n "redis"; service redis status; 
-echo "-------------------------"; 
-echo -n "redis.db.size:"; redis-cli dbsize; 
-redis-cli info memory | grep 'used.*human';
+echo -n "redis"; service redis status
+echo "-------------------------"
+echo "redis.db.size:$redisDbSize"
+echo "$redisStates"
