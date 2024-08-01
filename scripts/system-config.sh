066dbeefdfeb4667a105ca3f6762fc8c4bb72cfa
Ahmed Sabry <40901075+BenSabry@users.noreply.github.com>
2024-08-01 22:44:37 +0300

    Config revamp

--- scripts/system-config.sh
+++ scripts/system-config.sh
@@ -3,7 +3,7 @@ echo ""
 echo "$(date) $0 $@"
 
 # repositories
-repo="https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community/"
+repo="https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community"
 if ! (cat /etc/apk/repositories | grep "$repo" -q); then
     echo "$repo" >> /etc/apk/repositories
 fi
