88e6224ff0ce74e7a9ed21255d1720886e414acc
Ahmed Sabry <40901075+BenSabry@users.noreply.github.com>
2024-08-13 14:27:55 +0300

    config & optimizations

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
 # variables
-unboundStates="$(unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num)"
-unboundCachePercent="$(echo "$(unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num.cachehits= | cut -c 21-)/$(unbound-control -c /etc/unbound/unbound.conf stats_noreset | grep total.num.queries= | cut -c 19-)" | bc -l | cut -c 2-3;)"
+msg="$(unbound-control -c /etc/unbound/unbound.conf stats_noreset)"
+filters="total.num.queries=|total.num.cachehits|total.num.cachemiss|total.num.prefetch|total.num.recursivereplies|total.num.expired|total.recursion"
 
-redisStates="$(redis-cli info memory | grep 'used.*human')";
-redisDbSize="$(redis-cli dbsize)"
+uStats="$(echo "$msg" | grep -E "$filters")"
+uPerc="$(echo "$(echo "$uStats" | grep 'total.num.cachehits=' | cut -c 21-)/$(echo "$uStats" | grep total.num.queries= | cut -c 19-)" | bc -l | cut -c 2-3;)"
 
-# messages
+rStats="$(redis-cli info memory | grep 'used.*human')";
+rDbSize="$(redis-cli dbsize)"
+
+# # messages
 echo "https://github.com/BenSabry/UnboundRedis"
 echo ""
 
 echo -n "unbound"; service unbound status
 echo "-------------------------"
-echo "total.percent.cachehits=$unboundCachePercent%"
-echo "$unboundStates"
+echo "total.percent.cachehits=$uPerc%"
+echo "$uStats"
 echo ""
 
 echo -n "redis"; service redis status
 echo "-------------------------"
-echo "redis.db.size:$redisDbSize"
-echo "$redisStates"
+echo "redis.db.size:$rDbSize"
+echo "$rStats"
